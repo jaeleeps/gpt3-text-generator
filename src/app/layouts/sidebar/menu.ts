871595294077ccad3d0b83a137b35b963a0d525c
jaeleeps <jaeleeps@gmail.com>
2022-05-15 03:22:51 -0400

    edit menu items

--- src/app/layouts/sidebar/menu.ts
+++ src/app/layouts/sidebar/menu.ts
@@ -2,6 +2,21 @@ import { MenuItem } from './menu.model';
 
 export const MENU: MenuItem[] = [
     {
+      id: 1000,
+      label: 'MENUITEMS.TEXT_GENERATOR.TEXT',
+      isTitle: true
+    },
+    {
+      id: 1100,
+      label: 'MENUITEMS.TEXT_GENERATOR_PROMPT.TEXT',
+      icon: 'bx-edit-alt',
+    },
+    {
+      id: 1200,
+      label: 'MENUITEMS.TEXT_GENERATOR_ARCHIVE.TEXT',
+      icon: 'bx-archive',
+    },
+    {
         id: 80,
         label: 'MENUITEMS.COMPONENTS.TEXT',
         isTitle: true
