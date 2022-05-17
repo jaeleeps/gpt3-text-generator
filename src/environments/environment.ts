77111ae70112485d1c9d7db38213156e1fab8b16
jaeleeps <jaeleeps@gmail.com>
2022-05-18 00:37:04 +0900

    edit .gitignore env setting

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  defaultauth: "fackbackend",
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
+  gpt3_api_key: "",
+};
